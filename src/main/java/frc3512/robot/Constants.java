cf9ee1554118bcf495e4937f87e4d75c4af608fe
Adan Silva <adan.t.silva77@gmail.com>
2024-01-18 21:59:11 -0800

    update to L4 speed and gear ratio

--- src/main/java/frc3512/robot/Constants.java
+++ src/main/java/frc3512/robot/Constants.java
@@ -20,22 +20,13 @@ public final class Constants {
 
     public static final double swerveDeadband = 0.1;
 
-    public static final double maximumSpeed = Units.feetToMeters(14.5);
+    public static final double maximumSpeed = Units.feetToMeters(19.0);
 
-    // Angle conversion factor is 360 / (GEAR RATIO * ENCODER RESOLUTION)
-    //  In this case the gear ratio is 12.8 motor revolutions per wheel rotation.
-    //  The encoder resolution per motor revolution is 1 per motor revolution.
     public static final double angleConversionFactor =
         SwerveMath.calculateDegreesPerSteeringRotation(12.8, 1);
 
-    // Motor conversion factor is (PI * WHEEL DIAMETER IN METERS) / (GEAR RATIO * ENCODER
-    // RESOLUTION).
-    //  In this case the wheel diameter is 4 inches, which must be converted to meters to get
-    // meters/second.
-    //  The gear ratio is 6.75 motor revolutions per wheel rotation.
-    //  The encoder resolution per motor revolution is 1 per motor revolution.
     public static final double driveConversionFactor =
-        SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(4), 6.75, 1);
+        SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(4), 5.14, 1);
   }
 
   public static final class GeneralConstants {
