3e6518245a473e95b0b256c624337041ff4dcd5f
Adan Silva <adan.t.silva77@gmail.com>
2024-02-10 16:31:10 -0800

    Fix up autos for alliance flipping to be functional and clean up code

--- src/main/java/frc3512/robot/auton/Autos.java
+++ src/main/java/frc3512/robot/auton/Autos.java
@@ -2,102 +2,74 @@ package frc3512.robot.auton;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
-import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc3512.robot.Constants;
 import frc3512.robot.subsystems.Superstructure;
 import frc3512.robot.subsystems.Swerve;
-import java.util.HashMap;
 
 public class Autos {
 
   private final Superstructure superstructure;
   private final Swerve swerve;
   private final SendableChooser<Command> autonChooser;
-  private final HashMap<String, Command> eventMap;
 
   public Autos(Superstructure superstructure, Swerve swerve) {
     this.superstructure = superstructure;
     this.swerve = swerve;
 
-    eventMap = new HashMap<>();
     setMarkers();
 
-    NamedCommands.registerCommand("Reset Gyro", resetGyro());
-
     AutoBuilder.configureHolonomic(
-        swerve::getPose, // Robot pose supplier
-        swerve
-            ::resetOdometry, // Method to reset odometry (will be called if your auto has a starting
-        // pose)
-        swerve::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-        swerve::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-        new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in
-            // your Constants class
-            new PIDConstants(0.0020645, 0.0, 0.0), // Translation PID constants
-            new PIDConstants(0.01, 0.0, 0.0), // Rotation PID constants
-            3.5, // Max module speed, in m/s
-            0.4, // Drive base radius in meters. Distance from robot center to furthest module.
-            new ReplanningConfig(
-                false, false) // Default path replanning config. See the API for the options
-            // here
-            ),
+        swerve::getPose,
+        swerve::resetOdometry,
+        swerve::getRobotVelocity,
+        swerve::setChassisSpeeds,
+        new HolonomicPathFollowerConfig(
+            Constants.AutonConstants.TRANSLATION_PID,
+            Constants.AutonConstants.ANGLE_PID,
+            4.5,
+            swerve.getSwerveDriveConfiguration().getDriveBaseRadiusMeters(),
+            new ReplanningConfig()),
         () -> {
-          // Boolean supplier that controls when the path will be mirrored for the red alliance
-          // This will flip the path being followed to the red side of the field.
-          // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-          // var alliance = DriverStation.getAlliance();
-          // if (alliance.isPresent()) {
-          //  return alliance.get() == DriverStation.Alliance.Blue;
-          // }
-          return false;
+          var alliance = DriverStation.getAlliance();
+          return alliance.isPresent() ? alliance.get() == DriverStation.Alliance.Red : false;
         },
-        swerve // Reference to this subsystem to set requirements
-        );
+        swerve);
 
     autonChooser = new SendableChooser<Command>();
-    autonChooser.setDefaultOption("No-Op", new InstantCommand());
-    autonChooser.addOption("L Path", lPath());
-    autonChooser.addOption("Straight", straight());
-    autonChooser.addOption("Square", square());
-    autonChooser.addOption("Spin", spin());
+    autonChooser.setDefaultOption("No-op", new InstantCommand());
+
+    // Add autos
+    buildAuto("L Path");
+    buildAuto("Straight");
+    buildAuto("Circle");
 
     SmartDashboard.putData("Auton Chooser", autonChooser);
   }
 
-  private void setMarkers() {
-    eventMap.put("Reset Gyro", resetGyro());
-  }
-
-  public Command resetGyro() {
-    return new InstantCommand(() -> swerve.zeroGyro());
-  }
-
   public Command getSelected() {
-    swerve.zeroGyro();
     return autonChooser.getSelected();
   }
 
-  public Command lPath() {
-    // return AutoBuilder.buildAuto("Forward");
-    return AutoBuilder.followPath(PathPlannerPath.fromPathFile("L path"));
+  private void setMarkers() {
+    NamedCommands.registerCommand(
+        "Reset Gyro", new InstantCommand(() -> swerve.zeroGyroWithAlliance()));
   }
 
-  public Command straight() {
-    return AutoBuilder.buildAuto("Straight");
-  }
-
-  public Command square() {
-    return AutoBuilder.followPath(PathPlannerPath.fromPathFile("Square"));
-  }
-
-  public Command spin() {
-    return AutoBuilder.followPath(PathPlannerPath.fromPathFile("Spin Path"));
+  /*
+   * Builds the auto and adds it into the chooser for you.
+   * Use this method to save some time plz. :)
+   *
+   * @param autoName  Name of the auto file
+   */
+  private void buildAuto(String autoName) {
+    Command autoCommand = AutoBuilder.buildAuto(autoName);
+    autonChooser.addOption(autoName, autoCommand);
   }
 }
