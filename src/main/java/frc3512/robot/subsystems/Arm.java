c0500deff97bc6a69fc0a03fc547929ee2b0731d
Christopher <fireswordchris@gmail.com>
2024-02-17 11:02:44 -0800

    Readded the Subsystem files, not tested yet

---
+++ src/main/java/frc3512/robot/subsystems/Arm.java
@@ -0,0 +1,49 @@
+package frc3512.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc3512.lib.util.CANSparkMaxUtil;
+import frc3512.lib.util.CANSparkMaxUtil.Usage;
+import frc3512.robot.Constants;
+
+public class Arm extends SubsystemBase {
+    private CANSparkMax leftMotor = new CANSparkMax(14, MotorType.kBrushless);
+    private CANSparkMax rightMotor = new CANSparkMax(15, MotorType.kBrushless);
+    private DutyCycleEncoder armEncoder = new DutyCycleEncoder(5);
+
+    public Arm() {
+        leftMotor.restoreFactoryDefaults();
+        rightMotor.restoreFactoryDefaults();
+
+        CANSparkMaxUtil.setCANSparkMaxBusUsage(leftMotor, Usage.kMinimal);
+        CANSparkMaxUtil.setCANSparkMaxBusUsage(rightMotor, Usage.kPositionOnly);
+
+        leftMotor.setSmartCurrentLimit(Constants.ArmConstants.currentLimit);
+        rightMotor.setSmartCurrentLimit(Constants.ArmConstants.currentLimit);
+
+        leftMotor.setIdleMode(IdleMode.kCoast);
+        rightMotor.setIdleMode(IdleMode.kCoast);
+
+        rightMotor.setInverted(true);
+        rightMotor.follow(leftMotor);
+
+        rightMotor.burnFlash();
+        leftMotor.burnFlash();
+
+        leftMotor.set(0);
+
+        SmartDashboard.putNumber("Arm Encoder Distance Per Rotation", armEncoder.getDistancePerRotation());
+        SmartDashboard.putNumber("Arm Encoder", armEncoder.getAbsolutePosition());
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Arm Encoder Distance Per Rotation", armEncoder.getDistancePerRotation());
+        SmartDashboard.putNumber("Arm Encoder", armEncoder.getAbsolutePosition());
+    }
+}
---
+++ src/main/java/frc3512/robot/subsystems/Elevator.java
@@ -0,0 +1,38 @@
+package frc3512.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc3512.lib.util.CANSparkMaxUtil;
+import frc3512.lib.util.CANSparkMaxUtil.Usage;
+import frc3512.robot.Constants;
+
+public class Elevator extends SubsystemBase{
+    private CANSparkMax elevatorMotor = new CANSparkMax(13, MotorType.kBrushless);
+    private DutyCycleEncoder elevatorEncoder = new DutyCycleEncoder(1);
+
+    public Elevator() {
+        elevatorMotor.restoreFactoryDefaults();
+
+        CANSparkMaxUtil.setCANSparkMaxBusUsage(elevatorMotor, Usage.kPositionOnly);
+
+        elevatorMotor.setIdleMode(IdleMode.kCoast);
+        elevatorMotor.setSmartCurrentLimit(Constants.ElevatorConstants.currentLimit);
+        elevatorMotor.enableVoltageCompensation(10);
+
+        elevatorMotor.burnFlash();
+
+        elevatorMotor.set(0);
+
+        SmartDashboard.putNumber("Elevator Encoder", elevatorEncoder.getAbsolutePosition());
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Elevator Encoder", elevatorEncoder.getAbsolutePosition());
+    }
+}
